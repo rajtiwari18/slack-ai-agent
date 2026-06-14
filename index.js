32a20b9c07dfcbda8c5ccbce9ec5741ff9c4fdc9
Raj tiwari <00rajtiwari00@gmail.com>
2026-06-14 22:00:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ class SlackAIAgent {
         this.slack = new App({
             token: process.env.SLACK_BOT_TOKEN,
             signingSecret: process.env.SLACK_SIGNING_SECRET,
-            appToken: process.env.SLACK_APP_TOKEN
+            appToken: process.env.SLACK_APP_TOKEN,
+            socketMode: true
         });
         this.webClient = new WebClient(process.env.SLACK_BOT_TOKEN);
         this.openai = new ChatOpenAI({
